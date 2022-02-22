6b5d04820bd846d91331a042f49e2ff75d1ef4f5
2011brunofernandes <rgr.alexandre@tokiomarine.com.br>
2022-02-22 19:37:04 -0300

    paginacao

--- functions.php
+++ functions.php
@@ -22,6 +22,18 @@ function obtener_post($post_por_pagina, $conexion){
     $sentencia->execute();
     return $sentencia->fetchAll();
 }
+
+
+
+
+function numero_paginas($post_por_pagina, $conexion){
+    $total_post = $conexion->prepare("SELECT FOUND_ROWS() as total");
+    $total_post->execute();
+    $total_post = $total_post->fetch()['total'];
+    $numero_paginas = ($total_post / $post_por_pagina);
+    return $numero_paginas;
+
+}
 function id_articulo($id){
     return (int)limpiarDatos($id);
 }
